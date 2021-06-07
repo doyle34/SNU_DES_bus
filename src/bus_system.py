92ca6fe0cebc909b9f8931e5a0908b5c91da6ed8
doyle34 <parkdi0409@gmail.com>
2021-06-07 15:36:28 +0900

    added passenger waiting time tracking modified class Passenger moved bus_iat_dist member from class Bus to class Station during simulation, distribution changes as time flows, and monitor statistics (ex. passenger waiting time) per hour. monitored statistics are exported via csv file. new functions: - dist_change: change distribution with times - monitor: get statistics at every 60 minutes during simulation

--- src/bus_system.py
+++ src/bus_system.py
@@ -1,18 +1,20 @@
 import simpy
+import math
+import numpy as np
 from random import normalvariate, expovariate
 
 
 class Bus:
-    def __init__(self, env, name, bus_cap, stations, iat_dist_list):
+    def __init__(self, env, name, bus_cap, stations):
         self.env = env
         self.name = name
         self.station_idx = 0
         self.stations = stations
+        self.bus_idt_dist = np.array([13.5, 2.475])
         self.passengers = simpy.Store(self.env, capacity=bus_cap)
         self.psn_cnt = 0
-        self.iat_dist = iat_dist_list
         self.driving_process = self.env.process(self.drive())
-        self.driving_time = 0 ###################################
+        self.driving_time = 0
 
     def arrive(self, station):
         # bus arrives at a station, and passengers get off
@@ -35,11 +37,17 @@ class Bus:
                 print(f'{self.name} is full, so passengers in {station.name} renege')
                 current_len = len(station.boarding_queue.items)
                 while len(station.boarding_queue.items) < current_len / 2:
-                    yield station.boarding_queue.get()  # half of passengers in boarding queue leaves
+                    passenger_renege = yield station.boarding_queue.get()  # half of passengers in boarding queue leaves
+                    passenger_renege.renege(self.env)
+                    station.psn_waiting_time.append(passenger_renege.waiting_time)
+                    station.n_psn_renege += 1
+
                 break  # forced while loop exit
 
             else:  # if bus is not full
                 passenger_now = yield station.boarding_queue.get()
+                passenger_now.board(self.env)
+                station.psn_waiting_time.append(passenger_now.waiting_time)
                 yield self.passengers.put(passenger_now)
                 temp_cnt += 1
                 self.psn_cnt += 1
@@ -48,29 +56,33 @@ class Bus:
 
     def drive(self):
         while True:
-            for station, iat in zip(self.stations, self.iat_dist):
-                driving_time_single = normalvariate(iat[0], iat[1]) ##################
+            for station in self.stations:
+                driving_time_single = normalvariate(station.bus_iat_dist[0], station.bus_iat_dist[1])
                 yield self.env.timeout(driving_time_single)  # bus moves to next station
-                self.driving_time += driving_time_single ################
+                self.driving_time += driving_time_single
                 print(f'{self.name} arrives at {station.name}: {self.env.now}')
                 yield self.env.process(self.arrive(station))  # bus arrives at station[i]
                 self.station_idx += 1
             # bus finishes driving for one cycle
             print(f'{self.name} finishes driving for one cycle')
-            yield self.env.timeout(1)  # wait 1 minute for next driving cycle
+            dispatch_time = normalvariate(self.bus_idt_dist[0], self.bus_idt_dist[1])
+            yield self.env.timeout(dispatch_time)
             print(f'{self.name} returns to first station')
             self.station_idx = 0
 
 
 class Station:
-    def __init__(self, env, name, psn_iat_dist, psn_idt_dist):
+    def __init__(self, env, name):
         self.env = env
         self.name = name
         self.is_terminal = False
         self.n_psn_depart = 0
+        self.n_psn_renege = 0
         self.boarding_queue = simpy.Store(self.env)
-        self.psn_iat_dist = psn_iat_dist
-        self.psn_idt_dist = psn_idt_dist
+        self.psn_waiting_time = []
+        self.psn_iat_dist = np.array([10, 0.01])
+        self.psn_idt_dist = np.array([10, 0.01])
+        self.bus_iat_dist = np.array([7, 0.1])
         self.arrival_process = self.env.process(self.passenger_arrive())
         self.departure_process = self.env.process(self.passenger_depart())
 
@@ -81,6 +93,7 @@ class Station:
             # new passenger arrives at this station
             psn_code = 'P' + str(cnt).zfill(3)
             passenger = Passenger(self.name + psn_code)
+            passenger.start_waiting(self.env)
             yield self.boarding_queue.put(passenger)
             cnt += 1
 
@@ -94,9 +107,60 @@ class Station:
 class Passenger:
     def __init__(self, name):
         self.name = name
+        self.waiting_start = 0
+        self.waiting_end = 0
+        self.waiting_time = 0
+        self.reneged = False
+
+    def start_waiting(self, env):
+        self.waiting_start = env.now
+
+    def board(self, env):
+        self.waiting_end = env.now
+        self.waiting_time = self.waiting_end - self.waiting_start
+
+    def renege(self, env):
+        self.waiting_end = env.now
+        self.waiting_time = self.waiting_end - self.waiting_start
+        self.reneged = True
 
 
-def generate_buses(env, bus_capacity, stations, bus_iat_dist_list, idt_dist, n_bus, buses):
+def generate_buses(env, bus_capacity, stations, bus_idt_df, n_bus, buses):
     for i in range(n_bus):
-        yield env.timeout(normalvariate(idt_dist[0], idt_dist[1]))
-        buses.append(Bus(env, f'B{i}', bus_capacity, stations, bus_iat_dist_list))
+        j = math.floor(env.now / 60)
+        new_bus = Bus(env, f'B{i}', bus_capacity, stations)
+        new_bus.bus_idt_dist = np.array([bus_idt_df.iloc[j, 1], bus_idt_df.iloc[j, 2]])
+        buses.append(new_bus)
+        yield env.timeout(normalvariate(bus_idt_df.iloc[j, 1], bus_idt_df.iloc[j, 2]))
+
+
+def dist_change(env, stations, buses, df_list, temp_cnt):
+    time_zone = 18
+    psn_iat_df, psn_idt_df, bus_iat_df, bus_idt_df = df_list
+    while True:
+        for j in range(time_zone):
+            for i, station in enumerate(stations):
+                station.psn_iat_dist = np.array([psn_iat_df.iloc[i, j + 1], 0.01])
+                station.psn_idt_dist = np.array([psn_idt_df.iloc[i, j + 1], 0.01])
+                station.bus_iat_dist = np.array([bus_iat_df.iloc[i, j + 1], 0.1])
+
+            for bus in buses:
+                bus.bus_idt_dist = np.array([bus_idt_df.iloc[j, 1], bus_idt_df.iloc[j, 2]])
+
+            temp_cnt.append(0)
+            yield env.timeout(60)
+
+
+def monitor(env, stations, waiting_times):
+    while True:
+        yield env.timeout(60)
+        waiting_times_col = []
+        for station in stations:
+            if len(station.psn_waiting_time) > 0:
+                average_waiting_time = sum(station.psn_waiting_time) / len(station.psn_waiting_time)
+            else:
+                average_waiting_time = 0
+
+            waiting_times_col.append(average_waiting_time)
+
+        waiting_times.append(waiting_times_col)
--- src/run_simulation.py
+++ src/run_simulation.py
@@ -1,50 +1,64 @@
 import simpy
-from bus_system import Bus, Station, Passenger, generate_buses
+from bus_system import *
 from random import normalvariate, expovariate
 import numpy as np
 import pandas as pd
+import csv
 
 # system configuration
 n_stations = 9
 n_bus = 6
+time_zone = 18
 bus_capacity = 50
-SIM_TIME = 180
+SIM_TIME = 60 * time_zone + 1
 
 #
-total_passengers = 0 ###########################
-total_driving_time = 0 ##########################
+total_passengers = 0
+total_driving_time = 0
 
 # list of passenger arrival time distribution (mean, std) for each stations.
-psn_iat_df = pd.read_csv("Passenger_DATA_v2/psn_iat.csv")
-psn_iat_dist_list = [np.array([psn_iat_df["average"][i], 0]) for i in range(n_stations)]
+psn_iat_df = pd.read_csv("passenger_data/psn_iat.csv")
 # list of passenger departure time distribution (mean, std) for each stations
-psn_idt_df = pd.read_csv("Passenger_DATA_v2/psn_idt.csv")
-psn_idt_dist_list = [np.array([psn_idt_df["average"][i], 0]) for i in range(n_stations)]
+psn_idt_df = pd.read_csv("passenger_data/psn_idt.csv")
 # bus IAT distribution (mean, std) between stations
-bus_iat_df = pd.read_csv("bus_data_v2/bus_iat.csv")
-bus_iat_dist_list = [np.array([bus_iat_df["average"][i], bus_iat_df["std"][i]]) for i in range(n_stations)]
+bus_iat_df = pd.read_csv("bus_data/bus_iat.csv")
 # bus inter-dispatch time distribution (mean, std)
-bus_idt_df = pd.read_csv("bus_data_v2/bus_idt.csv")
-bus_idt_dist_list = [np.array([bus_idt_df["mean"][i], bus_idt_df["std"][i]]) for i in range(18)]
+bus_idt_df = pd.read_csv("bus_data/bus_idt.csv")
 
-# average value of one day
-# bus_idt_dist = np.array([10, 0])
-bus_idt_dist = np.array([bus_idt_df["mean"][18], bus_idt_df["std"][18]])
-
+df_list = [psn_iat_df, psn_idt_df, bus_iat_df, bus_idt_df]
 
 env = simpy.Environment()
-stations = [Station(env, f'S{i}', piat, pidt)
-            for i, (piat, pidt) in enumerate(zip(psn_iat_dist_list, psn_idt_dist_list))]
+stations = [Station(env, f'S{i}')
+            for i in range(n_stations)]
 stations[-1].is_terminal = True
 buses = []
-env.process(generate_buses(env, bus_capacity, stations, bus_iat_dist_list, bus_idt_dist, n_bus, buses))
+env.process(generate_buses(env, bus_capacity, stations, bus_idt_df, n_bus, buses))
+temp_cnt = []
+env.process(dist_change(env, stations, buses, df_list, temp_cnt))
+waiting_times = []
+env.process(monitor(env, stations, waiting_times))
 env.run(until=SIM_TIME)
 
-print('simulation end')
-for bus in buses:
-    print(f'{bus.name} transported {bus.psn_cnt} passengers during {bus.driving_time} min') ##############
-    total_passengers += bus.psn_cnt ######################################
-    total_driving_time += bus.driving_time ###############################
+print(f'simulation end')
+print(f'dist_change count = {len(temp_cnt)}')
 
-print(f'total passengers: {total_passengers}')############################
-print(f'total driving time: {total_driving_time}')############################
+for bus in buses:
+    print(f'{bus.name} transported {bus.psn_cnt} passengers during {bus.driving_time} min')
+    total_passengers += bus.psn_cnt
+    total_driving_time += bus.driving_time
+
+
+for station in stations:
+    if len(station.psn_waiting_time) > 0:
+        average_waiting_time = sum(station.psn_waiting_time) / len(station.psn_waiting_time)
+    else:
+        average_waiting_time = 0
+
+    print(f'average passenger waiting time at {station.name}: {round(average_waiting_time, 2)}')
+    print(f'{station.n_psn_renege} passengers reneged at {station.name}')
+
+print(f'total passengers: {total_passengers}')
+print(f'total driving time: {total_driving_time}')
+df_columns = psn_idt_df.columns.drop(['station', 'average', 'std'])
+waiting_times_df = pd.DataFrame(np.transpose(np.array(waiting_times)), columns=df_columns)
+waiting_times_df.to_csv('output/waiting_times.csv', sep=',')
