9df26806e4f0cf1d67fb44b17dbceb6ad48f9e34
thomasjh0 <thomas_jh@naver.com>
2021-06-15 17:04:20 +0900

    many bus_idt cases in one simulation

--- src/run_simulation.py
+++ src/run_simulation.py
@@ -13,15 +13,17 @@ best_case = -1
 best_profit = -100000000
 best_waiting_time = -1
 best_renege = -1
+best_waiting_cost_waste = -1
 best_net_profit = -1
 
 
 for case in range(n_cases):
 
     total_profit = 0
-    total_wating_time = 0
+    total_waiting_time = 0
     total_renege = 0
-    total_net_profit =0
+    total_net_profit = 0
+    total_waiting_cost_waste = 0
 
     for day in range(n_days):
         print(f'-------------------- case {case + 1} day {day + 1} --------------------')
@@ -114,53 +116,61 @@ for case in range(n_cases):
 #        print(f'sum of waiting time: {daily_total_waiting_time} minutes')
         daily_average_waiting_time = daily_total_waiting_time / daily_passengers
 #        print(f'daily average waiting time: {daily_average_waiting_time} minutes')
-        total_wating_time += daily_average_waiting_time
+        total_waiting_time += daily_average_waiting_time
 
         daily_waiting_cost_waste = daily_total_waiting_time * 66.8379
         daily_net_profit = daily_profit - daily_waiting_cost_waste
-        # print(f'net profit concidering wating time: {round(daily_net_profit)}')
+        # print(f'net profit concidering waiting time: {round(daily_net_profit)}')
+        total_waiting_cost_waste += daily_waiting_cost_waste
         total_net_profit += daily_net_profit
 
-    average_profit = total_profit / n_days
-    average_waiting_time = total_wating_time / n_days
+
+    average_profit = total_profit/n_days
+    average_waiting_time = total_waiting_time / n_days
     average_renege = total_renege / n_days
+    average_waiting_cost_waste = total_waiting_cost_waste / n_days
     average_net_profit = total_net_profit / n_days
     print('')
     print(f'{n_days}days average profit: {average_profit} won')
     print(f'{n_days}days average waiting: {average_waiting_time} minutes')
     print(f'{n_days}days average renege: {average_renege}')
+    print(f'{n_days}days average waste: {average_waiting_cost_waste} won')
     print(f'{n_days}days average net profit: {average_net_profit} won')
 
     # append results to final_results.csv
-    final_results[0].append(case)
+    final_results[0].append(case + 1)
     final_results[1].append(average_profit)
     final_results[2].append(average_waiting_time)
     final_results[3].append(average_renege)
+    # final_results[3].append(average_waiting_cost_waste)
     final_results[4].append(average_net_profit)
 
     if average_profit > best_profit:
-        best_case = case
+        best_case = case + 1
         best_profit = average_profit
         best_waiting_time = average_waiting_time
         best_renege = average_renege
+        best_waiting_cost_waste = average_waiting_cost_waste
         best_net_profit = average_net_profit
 
 print('')
-print(f'best case : case{best_case + 1}')
+print(f'best case : case{best_case}')
 print(f'average profit: {best_profit} won')
 print(f'average waiting: {best_waiting_time} minutes')
 print(f'average renege: {best_renege}')
+print(f'average net profit: {best_waiting_cost_waste}')
 print(f'average net profit: {best_net_profit}')
 
 final_results[0].append('best : ' + str(best_case))
 final_results[1].append(best_profit)
 final_results[2].append(best_waiting_time)
 final_results[3].append(best_renege)
+# final_results[3].append(average_waiting_cost_waste)
 final_results[4].append(best_net_profit)
 
 
 # print final results to csv file
-# result_col = ['case', 'average profit', 'average wating time', 'average renege']
-result_col = ['case', 'average profit', 'average wating time', 'average renege', 'average net profit']
+# result_col = ['case', 'average profit', 'average waiting time', 'average renege']
+result_col = ['case', 'average profit', 'average waiting time', 'average renege', 'average net profit']
 result_df = pd.DataFrame(np.transpose(np.array(final_results)), columns=result_col)
 result_df.to_csv('output/final_results.csv', sep=',', index=False)
